2661226125f0fb5611e6d91a3b668f18665e0095
Olalekan Owolabi <141418238+lekxy246@users.noreply.github.com>
2025-05-20 20:34:45 +0100

    Update salestrend.sql

--- salestrend.sql
+++ salestrend.sql
@@ -1,6 +1,6 @@
 ---- sales Trend -------
 
-select hour(order_time) as hour, count(hour(order_time)) as hours
+select hour(order_time) as hour, count(hour(order_time)) as sum_of_order_Number
 from pizza_sales
 group by hour(order_time)
 order by hour(order_time) asc;
